b81715bdc2918d94a560835b9e10b606cb65f80b
pengkemaze <1223316899@qq.com>
2018-09-02 22:37:42 +0800

    设置路由导航

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,10 +1,30 @@
 <template>
     <div class="col-sm-3 col-md-2 sidebar">
         <ul class="nav nav-sidebar">
-          <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Reports</a></li>
-          <li><a href="#">Analytics</a></li>
-          <li><a href="#">Export</a></li>
+          <!-- <li class="active">
+            <a href="#">Overview <span class="sr-only">(current)</span></a>
+          </li>
+          <li>
+            <a href="#">Reports</a>
+          </li>
+          <li>
+           <a href="#">Analytics</a>
+          </li>
+          <li>
+            <a href="#">Export</a>
+          </li> -->
+          <!-- router-link 的 tag 属性 标志将生成 li 标签
+            exact-active-class="active" 绑定样式 使标签高亮
+          -->
+          <router-link tag="li" to="/heroes">
+            <a>英雄管理</a>
+          </router-link>
+          <router-link tag="li" to="/weapons">
+            <a>武器管理</a>
+          </router-link>
+          <router-link tag="li" to="/equips">
+            <a>装备管理</a>
+          </router-link>
         </ul>
     </div>
 </template>
